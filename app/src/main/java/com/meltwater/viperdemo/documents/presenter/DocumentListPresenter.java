72842eb0947ad9eb7774006da47925b89dafa4b4
Marco Galicia <marco.galicia@meltwater.com>
2017-12-05 18:17:54 -0600

    nothing changed

--- app/src/main/java/com/meltwater/viperdemo/documents/presenter/DocumentListPresenter.java
+++ app/src/main/java/com/meltwater/viperdemo/documents/presenter/DocumentListPresenter.java
@@ -1,11 +1,9 @@
 package com.meltwater.viperdemo.documents.presenter;
 
-import android.support.v7.app.AppCompatActivity;
 
 import com.meltwater.viperdemo.documents.DocumentListContract;
 import com.meltwater.viperdemo.documents.entity.DocumentModel;
 import com.meltwater.viperdemo.documents.interactor.DocumentListInteractor;
-import com.meltwater.viperdemo.documents.router.DocumentListRouter;
 import com.meltwater.viperdemo.documents.view.BaseViperView;
 
 import java.util.List;
@@ -14,7 +12,7 @@ import java.util.List;
  * Created by thinhnguyen on 12/3/17.
  */
 
-public class DocumentListPresenter extends BasePresenter<BaseViperView>  implements DocumentListContract.Presenter, DocumentListContract.InteractorOutput {
+public class DocumentListPresenter extends BasePresenter<BaseViperView> implements DocumentListContract.Presenter, DocumentListContract.InteractorOutput {
 
     private DocumentListInteractor interactor;
 
